b77a6bf8f9fb319d8f9f405001e80aba52e98ffa
Nesswit <rishubil@gmail.com>
2017-06-16 23:28:58 +0900

    Add ebook-reader dccon

---
+++ js/dccon_list.js
@@ -0,0 +1,11 @@
+var dcconList = [
+  {'keywords' : ['우리핵박수', '우리핵박수1',], 'tags': ['우리핵', '움짤', '박수'], 'path': 'images/dccon/ebook-reader/우리핵박수1.gif',},
+  {'keywords' : ['우리핵박수2',], 'tags': ['우리핵', '움짤', '박수'], 'path': 'images/dccon/ebook-reader/우리핵박수2.gif',},
+  {'keywords' : ['우리핵박수3',], 'tags': ['우리핵', '움짤', '박수'], 'path': 'images/dccon/ebook-reader/우리핵박수3.gif',},
+  {'keywords' : ['우리핵안녕',], 'tags': ['우리핵', '움짤', '인사'], 'path': 'images/dccon/ebook-reader/우리핵안녕.gif',},
+  {'keywords' : ['우리핵아닌데',], 'tags': ['우리핵', '움짤',], 'path': 'images/dccon/ebook-reader/우리핵아닌데.gif',},
+  {'keywords' : ['우리핵투정',], 'tags': ['우리핵', '움짤',], 'path': 'images/dccon/ebook-reader/우리핵투정.gif',},
+  {'keywords' : ['우리핵드럼',], 'tags': ['우리핵', '움짤',], 'path': 'images/dccon/ebook-reader/우리핵드럼.gif',},
+  {'keywords' : ['우리핵쌍안경',], 'tags': ['우리핵', '움짤',], 'path': 'images/dccon/ebook-reader/우리핵쌍안경.gif',},
+  {'keywords' : ['우리핵빵야',], 'tags': ['우리핵', '움짤',], 'path': 'images/dccon/ebook-reader/우리핵빵야.gif',},
+];
