4397a4d6d62df8b3c35f43d948ffa37b58c97ceb
Nesswit <rishubil@gmail.com>
2017-08-20 17:10:33 +0900

    Add chat page

--- source/src/router/index.js
+++ source/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import List from '@/components/list/List';
+import Chat from '@/components/chat/Chat';
 
 Vue.use(Router);
 
@@ -8,5 +9,6 @@ export default new Router({
   routes: [
     {path: '/', name: 'index', redirect: {name: 'list'}},
     {path: '/list', name: 'list', component: List, props: route => ({dccon_list_url: route.query.dccon_list})},
+    {path: '/chat', name: 'chat', component: Chat, props: route => ({dccon_list_url: route.query.dccon_list})},
   ],
 });
